f1eb793891082d1ffc670890529fdfffdbe22038
xEdwardP <edwardjpineda@outlook.com>
2026-03-04 18:58:38 -0600

    feat: initialize frontend with Vite and React setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const {PrismaClient} = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const prisma = new PrismaClient();
@@ -9,61 +9,73 @@ app.use(cors());
 app.use(express.json());
 
 // List
-app.get("/api/products", async(req, res) => {
-    const items = prisma.product.findMany({orderBy: {id: "desc"}});
+app.get("/api/products", async (req, res) => {
+  const items = await prisma.product.findMany({ orderBy: { id: "desc" } });
+  res.json(items);
 });
 
 // Get One
-app.get("/api/products/:id", async(req, res) => {
-    const id =  Number(req.params.id);
-    const item = await prisma.product.findUnique({where: {id}});
-    if (!item) return res.status(400).json({message: "Not Found"});
-    res.json(item);
+app.get("/api/products/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const item = await prisma.product.findUnique({ where: { id } });
+  if (!item) return res.status(404).json({ message: "Not Found" });
+  res.json(item);
 });
 
 // Create
-app.post("/api/products", async(req, res) => {
-    const {name, description, price} = req.body;
-    if (!name || price == undefined){
-        return res.status(400).json({message: "Name and price are required."});
-    }
+app.post("/api/products", async (req, res) => {
+  const { name, description, price } = req.body;
+  if (!name || price == undefined) {
+    return res.status(400).json({ message: "Name and price are required." });
+  }
 
-    const created = await prisma.product.create({
-        data: {
-            name: String(name).trim(),
-            description: description ? String(description).trim() : null,
-            price: Number(price),
-        },
-    });
+  const created = await prisma.product.create({
+    data: {
+      name: String(name).trim(),
+      description: description ? String(description).trim() : null,
+      price: Number(price),
+    },
+  });
 
-    res.status(201).json(created);
+  res.status(201).json(created);
 });
 
 // Update
-app.put("/api/products/:id", async(req, res) => {
-    const id =  Number(req.params.id);
-    const {name, description, price} = req.body;
+app.put("/api/products/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const { name, description, price } = req.body;
 
+  try {
     const updated = await prisma.product.update({
-        where: {id},
-        data: {
-            name: name === undefined ? undefined: String(name).trim(),
-            description: description === undefined ? undefined: {description} ? String(description).trim() : null,
-            price: price === undefined ? undefined: Number(price),
-        },
+      where: { id },
+      data: {
+        name: name === undefined ? undefined : String(name).trim(),
+        description:
+          description === undefined
+            ? undefined
+            : description
+              ? String(description).trim()
+              : null,
+        price: price === undefined ? undefined : Number(price),
+      },
     });
 
     res.json(updated);
+  } catch (error) {
+    res.status(404).json({ message: "Not Found" });
+  }
 });
 
 // Delete
-app.delete("/api/products/:id", async(req, res) => {
-    const id =  Number(req.params.id);
+app.delete("/api/products/:id", async (req, res) => {
+  const id = Number(req.params.id);
 
-    try {
-        await prisma.product.delete({where: {id}});
-        res.status(204).send();
-    } catch (error) {
-        res.status(404).json({message: "Not Found"});
-    }
+  try {
+    await prisma.product.delete({ where: { id } });
+    res.status(204).send();
+  } catch (error) {
+    res.status(404).json({ message: "Not Found" });
+  }
 });
+
+app.listen(4000, () => console.log("Api en http://localhost:4000"));
